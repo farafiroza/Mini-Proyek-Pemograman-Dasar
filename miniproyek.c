8f542825117e18bf852f0b6dbb90d4e14d354fd2
Fara Firoza <farafiroza3@gmail.com>
2025-05-22 09:45:38 +0700

    Add files via upload

---
+++ miniproyek.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main(){
+// menyimpan data nama siswa 7b 8 orang dan maks 30 karakter 
+    char daftarNama[8][30] = {"fara", "ratna", "amira", "sinta", "leon", "sandi", "restu", "yuni"};
+
+// nama yang diinputkan uset maks 30 karakter
+    char namaInput[30];
+    float nilaiEsai, nilaiPG;
+    int percobaan = 0;
+
+// untuk loop pengecekan nama sampai nama benar
+  
+    while (1) {
+        printf("masukan nama siswa kelas 7b dengan huruf kecil:");
+        scanf("%s", namaInput);
+
+        int benar = 0;
+        for (int i = 0; i < 8; i++){
+            if(strcmp(namaInput, daftarNama[i]) == 0){
+                benar = 1;
+                break;
+            }
+
+        }
+    
+
+    // jikan nama yang diinput benar selanjutnya input nilai
+    if(benar){
+        break;
+    }
+
+    else{
+    percobaan++; 
+        printf("nama tidak ditemukan\n");
+
+    // membatasi percobaan hanya 3 kali
+    if(percobaan >=3) {
+
+        printf("nama tidak valid mencapai batas, program dihentikan\n");
+        return 0;
+        }
+    }
+}
+
+// menginputkan nilai
+    printf("masukan nilai esai: ");
+    scanf("%f", &nilaiEsai);
+
+    printf("masukan nilai pilihan ganda: ");
+    scanf("%f", &nilaiPG);
+
+    // menghitung total dan rata rata nilai
+    float total = nilaiEsai + nilaiPG;
+    float rataRata = total/2;
+
+    // menampilkan hasil
+    printf("\nnama = %s\n", namaInput);
+    printf("total nilai = %.2f\n", total);
+    printf("Rata - rata = %.2f\n", rataRata);
+
+    // program mengecek dari nilai apakah lulus atau remedial
+    if(rataRata >= 70) {
+        printf("LULUS\n");
+    } else {
+        printf("REMEDIAL\n");
+    }
+    return 0;
+}
